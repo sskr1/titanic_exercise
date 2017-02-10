c6e6fd584de258624e1e841daf744046060fccae
sskr1 <sumanasukumar@hotmail.com>
2017-02-10 20:55:39 +1100

    Add files via upload

---
+++ Rscript_titanic.R
@@ -0,0 +1,26 @@
+rm(list = ls())
+library(dplyr)
+library(tidyr)
+titanicnew = read.csv("titanic_original.csv", header = T, check.names = T, stringsAsFactors = T)
+str(titanicnew)
+titanic1 = titanicnew
+#find out number of blanks in Embarked column
+summary(titanicnew$embarked)
+titanicnew$embarked = sub("^$", "S",titanicnew$embarked)
+factor(titanicnew$embarked)
+summary(titanicnew$age)
+titanicnew$age[is.na(titanicnew$age)] = mean(titanicnew$age,trim = 0, na.rm = TRUE)
+summary(titanicnew$age)
+summary(titanicnew$boat)
+str(titanicnew$boat)
+summarise(titanicnew$boat)
+levels(titanicnew$boat)
+count(titanicnew$boat)
+as.character(titanicnew$boat)
+titanicnew$boat = sub("^$","NA", titanicnew$boat)
+levels(as.factor(titanicnew$boat))
+str(titanicnew$cabin)
+titanicnew$has_cabin_number = titanicnew$cabin
+titanicnew$has_cabin_number = ifelse(grepl("[[alnum:]]",titanicnew$has_cabin_number),1,0)
+titanicnew$has_cabin_number
+write.csv(titanicnew, file ="titanic_clean.csv")
